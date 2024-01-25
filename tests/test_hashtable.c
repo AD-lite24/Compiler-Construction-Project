04ed7296a942a38fca14fb87af67ac7d9857e54b
Time-11-emiT <99984506+Time-11-emiT@users.noreply.github.com>
2024-01-26 02:47:36 +0530

    test_hashtable.c correct one

--- tests/test_hashtable.c
+++ tests/test_hashtable.c
@@ -1,7 +1,16 @@
 #include <unity.h>
 #include <stdio.h>
 #include "datastructures/hashtable.h"
-#include "test_hashtable.h"
+
+void setUp(void)
+{
+    // set up test environment, if needed
+}
+
+void tearDown(void)
+{
+    // clean up test environment, if needed
+}
 
 void test_create_table(void)
 {
@@ -17,46 +26,64 @@ void test_insert_and_retrieve(void)
 {
     HTABLE table = createTable();
 
-    htInsert(table, "value123");
+    char* values[] = {"value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10",
+                      "value11", "value12", "value13", "value14", "value15", "value16", "value17", "value18", "value19", "value20"};
 
-    int key = hashfunction("value123");
-    LL list = table->items[key];
-    NODE retrievedNode = list->head;
+    for (int i = 0; i < 20; i++) {
+        htInsert(table, values[i]);
+    }
 
-    TEST_ASSERT_NOT_NULL(retrievedNode);
-    TEST_ASSERT_EQUAL_STRING("value123", retrievedNode->item);
-
-    freeTable(table);
-}
-
-
-void test_insert_collision(void) {
-    htable *table = createTable();
-
-
-    htInsert(table, "value123");
-    htInsert(table, "value133");
-
-    int key1 = hashfunction("value123");
-    int key2 = hashfunction("value133");
-
-    LL list1 = table->items[key1];
-    LL list2 = table->items[key2];
-
-    NODE retrievedNode1 = list1->head;
-    NODE retrievedNode2 = list2->head;
-
-    TEST_ASSERT_NOT_NULL(retrievedNode1);
-    TEST_ASSERT_NOT_NULL(retrievedNode2);
-
-    TEST_ASSERT_EQUAL_STRING("value123", retrievedNode1->item);
-    TEST_ASSERT_EQUAL_STRING("value133", retrievedNode2->item);
+    for (int i = 0; i < 20; i++) {
+        int key = hashfunction(values[i]);
+        LL list = table->items[key];
+        NODE retrievedNode = list->head;
+        TEST_ASSERT_NOT_NULL(retrievedNode);
+        TEST_ASSERT_EQUAL_STRING(values[i], retrievedNode->item);
+    }
 
     freeTable(table);
 }
 
-void test_hash_table() {
+void test_insert_collision(void)
+{
+    HTABLE table = createTable();
+
+    char* values1[] = {"value1", "value2", "value3", "value4", "value5"};
+    char* values2[] = {"value6", "value7", "value8", "value9", "value10"};
+
+    for (int i = 0; i < 5; i++) {
+        htInsert(table, values1[i]);
+        htInsert(table, values2[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        int key1 = hashfunction(values1[i]);
+        int key2 = hashfunction(values2[i]);
+
+        LL list1 = table->items[key1];
+        LL list2 = table->items[key2];
+
+        NODE retrievedNode1 = list1->head;
+        NODE retrievedNode2 = list2->head;
+
+        TEST_ASSERT_NOT_NULL(retrievedNode1);
+        TEST_ASSERT_NOT_NULL(retrievedNode2);
+
+        TEST_ASSERT_EQUAL_STRING(values1[i], retrievedNode1->item);
+        TEST_ASSERT_EQUAL_STRING(values2[i], retrievedNode2->item);
+    }
+
+    freeTable(table);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    // Run the test cases
     RUN_TEST(test_create_table);
     RUN_TEST(test_insert_and_retrieve);
     RUN_TEST(test_insert_collision);
+
+    return UNITY_END();
 }
