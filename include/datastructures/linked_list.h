4aec6eee7909693d38b808af03f2ec4d674b88ca
Time-11-emiT <99984506+Time-11-emiT@users.noreply.github.com>
2024-01-26 02:30:58 +0530

    inked_list.h

---
+++ include/datastructures/linked_list.h
@@ -0,0 +1,35 @@
+#ifndef LINKED_L
+#define LINKED_L
+
+#include "datastructures/symbol_table_item.h"
+
+typedef struct Node {
+    node_item item;
+    struct Node* next;
+} node;
+
+typedef node* NODE;
+
+typedef struct LinkedList {
+    NODE head;
+    int count;
+} ll;
+typedef ll* LL;
+
+LL createNewList();
+
+NODE createNewNode(node_item data);
+
+void freeNode(NODE node);
+
+void insertNodeFirst(NODE node, LL list);
+
+void insertNodeLast(NODE node, LL list);
+
+void removeNodeFirst(LL list);
+
+void removeNodeLast(LL list);
+
+void freeLL(LL list);
+
+#endif
