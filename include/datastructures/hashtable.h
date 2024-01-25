67619e75b999479d31dd284c5b7ddd7070b4b359
Time-11-emiT <99984506+Time-11-emiT@users.noreply.github.com>
2024-01-26 02:32:00 +0530

    Update hashtable.h

--- include/datastructures/hashtable.h
+++ include/datastructures/hashtable.h
@@ -3,41 +3,6 @@
 
 #define HASHTABLE_SIZE 5000
 
-typedef struct Node {
-    char* item;
-    struct Node* next;
-} node;
-
-typedef node* NODE;
-
-typedef struct LinkedList {
-    NODE head;
-    int count;
-} ll;
-typedef ll* LL;
-
-LL createNewList();
-
-NODE createNewNode(char* data);
-
-void freeNode(NODE node);
-
-void insertNodeFirst(NODE node, LL list);
-
-void insertNodeLast(NODE node, LL list);
-
-void removeNodeFirst(LL list);
-
-void removeNodeLast(LL list);
-
-
-typedef struct h_item {
-    long long int key;
-    char value[30];
-} h_item;
-
-typedef h_item* H_ITEM;
-
 typedef struct hashtable {
     LL* items;
     int count;
@@ -45,18 +10,13 @@ typedef struct hashtable {
 
 typedef htable* HTABLE;
 
-H_ITEM createItem(long long int key, char *value);
-
 HTABLE createTable();
-
-// void printTable(HTABLE table);
 
 void htInsert(HTABLE table,char *value);
 
-void freeItem(H_ITEM item);
-
 void freeTable(HTABLE table);
+
+void freeList(LL list);
 
 int hashfunction(char* name);
 
-#endif 
