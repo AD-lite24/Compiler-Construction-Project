fc4fd5234d63c2060588e707232305adeb624cfe
AD-lite24 <f20210505@pilani.bits-pilani.ac.in>
2024-01-26 14:49:37 +0530

    added token types

--- include/symbol_table/symboltable.h
+++ include/symbol_table/symboltable.h
@@ -3,6 +3,13 @@
 
 #define MAXLEN 1024
 
+// token types
+#define NUM_TK 0
+#define R_NUM_TK 1
+#define RUID 2
+#define FUNID_TK 3
+#define FIELDID_TK 4
+#define ID_TK 5
 
 typedef struct LineReferenceList {
     int line;
