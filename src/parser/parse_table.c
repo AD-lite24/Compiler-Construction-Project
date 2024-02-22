2108b58011ebd937977a2b9fe9f5569d9a40aa0b
AD-lite24 <f20210505@pilani.bits-pilani.ac.in>
2024-02-22 16:48:35 +0530

    Completed parser table filler
    Co-authored-by: Rakshit Aggarwal <Raksh-Agg@users.noreply.github.com>
    Co-authored-by: Aneesh Kabra <AneeshKabra@users.noreply.github.com>

--- src/parser/parse_table.c
+++ src/parser/parse_table.c
@@ -1,60 +1,92 @@
-#include "parser/parse_table.h"
-#include "parser/parser.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "include/parser/parse_table.h"
 
-ProdRule **createParseTable(FirstAndFollow F, ProdRule **parseTable) {
-    FILE *fp = fopen("ModifiedGrammar.txt", "r");
-    char buff[100];
 
-    if (!fp) {
-        printf("Error opening file");
-        return 1;
+// ProdRule **createParseTable(FirstAndFollow F, ProdRule **parseTable) {
+//     FILE *fp = fopen("ModifiedGrammar.txt", "r");
+//     char buff[100];
+
+//     if (!fp) {
+//         printf("Error opening file");
+//         return 1;
+//     }
+
+//     while (fgets(buff, sizeof(buff), fp) != NULL) {
+
+//         buff[strcspn(buff, "\n")] = '\0';
+
+//         char *token;
+//         char *dash_position = strchr(buff, '-');
+
+//         *dash_position = '\0';
+//         char *first_part = buff;
+//         char *second_part = dash_position + 1;
+
+//         Elements x = stringToEnum(first_part); // non terminal LHS
+
+//         char *token = strtok(second_part, ";");
+
+//         while (token != NULL) {
+
+//             ProdRule p1;
+//             p1.LHS = x;
+//             trim(token);
+
+//             char *token1 = strtok(token, " ");
+//             int count = 0;
+//             Elements * x1;
+//             while (token1 != NULL) {
+//                 Elements y = stringToEnum(token1);
+//                 p1.RHS[count++] = y;
+
+//                 if (y != TK_EPSILON)
+//                     x1 = computeFirstSpecial(p1);
+//                 else
+//                     x1 = computeFollowSpecial(p1);
+//                 token1 = strtok(NULL, " ");
+//             }
+//             token = strtok(NULL, ";");
+
+//             for (int i = 0; i < sizeof(x1)/sizeof(Elements); i++) {
+//                 parseTable[x][x1[i]] = p1;
+//             }
+//         }
+//     }
+
+//     return parseTable;
+// }
+
+ProdRule convertLLtoProd(int i, NODE_LL rule) {
+    ProdRule ans;
+    ans.LHS = i;
+    NODE_ELE ptr = rule->item->head;
+    int index = 0;
+    while (ptr != NULL) {
+        ans.RHS[index++] = ptr->item;
+        ptr = ptr->next;
     }
+    return ans;
+}
 
-    while (fgets(buff, sizeof(buff), fp) != NULL) {
-
-        buff[strcspn(buff, "\n")] = '\0';
-
-        char *token;
-        char *dash_position = strchr(buff, '-');
-
-        *dash_position = '\0';
-        char *first_part = buff;
-        char *second_part = dash_position + 1;
-
-        NonTerminals x = stringToEnumNonTerm(first_part); // non terminal LHS
-
-        char *token = strtok(second_part, ";");
-
-        while (token != NULL) {
-
-            ProdRule p1;
-            p1.LHS = x;
-            trim(token);
-
-            char *token1 = strtok(token, " ");
-            int count = 0;
-            Terminals * x1;
-            while (token1 != NULL) {
-                Elements y = stringToEnum(token1);  
-                p1.RHS[count++] = y;
-                
-                if (y !=EPSILON)
-                    x1 = computeFirstSpecial(p1, grammar G);
-                else
-                    x1 = computeFollowSpecial(p1, grammar G);
-                token1 = strtok(NULL, " ");
+ProdRule **createParseTable(FirstAndFollow F, ProdRule **ParseTable) {
+    for (int i = 0; i < sizeof(grammar_glob) / sizeof(LL_LL); i++) {
+        NODE_LL rule = grammar_glob->rules[i]->head;
+        Elements *x;
+        while (rule != NULL) {
+            ProdRule prod_rule = convertLLtoProd(i, rule);
+            if (checkEpsilonInFirst(rule->item) == 1) {
+                x = computeFollowSpecial(prod_rule);
+            } else {
+                x = computeFirstSpecial(prod_rule);
             }
-            token = strtok(NULL, ";");
-
-            for (int i = 0; i < sizeof(x1)/sizeof(Terminals); i++) {
-                parseTable[x][x1[i]] = p1;
+            for (int j = 0; j < sizeof(x) / sizeof(Elements); j++) {
+                ParseTable[i][x[j] - NUM_NONTERMS] = prod_rule;
             }
-
-
+            rule = rule->next;
         }
     }
+    return ParseTable;
+}
 
-    return parseTable;
+void parseInputSourceCode(char *TestCaseFile, ProdRule **ParseTable) {
+
 }
---
+++ tests/test_parser_file.h
@@ -0,0 +1,5 @@
+#include "parser/parser.h"
+#include "parser/grammar.h"
+#include "parser/parse_table.h"
+
+void test_parser_file(char* filename);
